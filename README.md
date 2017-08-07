8af579ecb5e693d3c32352778344e66ea90ebe3f
don <lawtrooper@tp.edu.tw>
2017-08-07 11:17:06 +0800

    add README.md and adjust movie.js

---
+++ README.md
@@ -0,0 +1,10 @@
+# Node.js Video Converter
+為了解決可一次快速轉檔多部影片而製作
+
+1. 電腦環境要先安裝[ffmpeg](https://www.ffmpeg.org/)
+2. 使用 [node-fluent-ffmpeg](https://github.com/fluent-ffmpeg/node-fluent-ffmpeg) 套件包
+
+## 使用
+1. 將要轉檔的影片放入/videoIn資料夾
+2. 在CMD執行`node movie`
+3. 轉好的影片會在/videoOut資料夾
---
+++ movie.js
@@ -0,0 +1,42 @@
+
+const fs = require('fs');
+const ffmpeg = require('fluent-ffmpeg');
+const command = ffmpeg();
+
+//==========================
+//         setting
+//==========================
+//folder
+const inFolder = "./videoIn";
+const outFolder = "./videOut";
+//config
+const videoBitrateVal = 384;//int
+const audioBitrateVal = '128k';//str
+
+
+//get files from designated folder
+fs.readdirSync(inFolder).forEach(file => {
+  console.log(file + 'is ready to convert!');
+  convertFile(file);
+})
+
+
+//get the video file ,convert the video file, 
+//and save converted file to designated folder
+function convertFile(filename){
+  var proc = ffmpeg(inFolder + "\\" + filename)
+  //set ffmpeg.exe path
+  .setFfmpegPath("C:\\ffmpeg\\bin\\ffmpeg.exe")
+  // set video bitrate
+  .videoBitrate(384)
+  .audioBitrate('128k')
+
+  .on('end', function() {
+    console.log(filename + ' has been converted succesfully!');
+  })
+  .on('error', function(err) {
+    console.log('an error happened: ' + err.message);
+  })
+  // save to file
+  .save(outFolder + "\\" + filename);
+}
